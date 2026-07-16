887770c406d0fffd65d26e7b311a2bf2b413bf14
chitieu <hungbeu292005@gmail.com>
2026-07-16 21:47:24 +0700

    16/7 initial-download-file

--- file-service/src/main/java/com/devteria/file/repository/FileRepository.java
+++ file-service/src/main/java/com/devteria/file/repository/FileRepository.java
@@ -1,7 +1,10 @@
 package com.devteria.file.repository;
 
 import com.devteria.file.dto.FileInfo;
+import com.devteria.file.entity.FileMgmt;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.StringUtils;
@@ -46,4 +49,11 @@ public class FileRepository {
                 .url(urlPrefix + fileName)
                 .build();
     }
+
+    public Resource read(FileMgmt fileMgmt)throws IOException {
+        var data=Files.readAllBytes(Path.of(fileMgmt.getPath()));
+        return new ByteArrayResource(data);
+    }
+
+
 }
